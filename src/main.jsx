49157c041160303b52f3a92b1ab3dd808b99abd7
NaZhou2024 <182044461+NaZhou2024@users.noreply.github.com>
2025-11-29 10:15:28 -0600

    fix main two hooks issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./components/Login";
 import QuizPage from "./components/QuizPage";
 import TopicList from "./components/TopicList";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/topics" element={<TopicList />} />
-      <Route path="/quiz/:topicId" element={<QuizPage />} />
-    </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/topics" element={<TopicList />} />
+        <Route path="/quiz/:topicId" element={<QuizPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 );
